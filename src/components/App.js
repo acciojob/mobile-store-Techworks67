becd6a50117f67897e2ca26f1e747e40785a0fd1
Techworks67 <166513600+Techworks67@users.noreply.github.com>
2026-09-06 03:38:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
-
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './../styles/App.css';
+import { ProductsProvider } from "./ProductsContext";
+import ProductList from "./ProductList";
+import ProductDetails from "./ProductDetails";
+import AdminPanel from "./AdminPanel";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ProductsProvider>
+        <Router>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/admin">Admin Panel</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={ProductList} />
+            <Route path="/admin" component={AdminPanel} />
+            <Route path="/products/:id" component={ProductDetails} />
+          </Switch>
+        </Router>
+      </ProductsProvider>
     </div>
   )
 }
